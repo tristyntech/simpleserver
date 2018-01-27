e87b08459141853e05e81902083c29872e2ce4fb
Tristyn Leos <tristyntech@gmail.com>
2018-01-27 01:10:17 -0600

    ready for deployment

--- server.js
+++ server.js
@@ -1,47 +1,95 @@
 //  OpenShift sample Node application
 var express = require('express'),
     app     = express(),
-    morgan  = require('morgan');
+    morgan  = require('morgan'),
+    mongoose = require('mongoose');
 
-Object.assign=require('object-assign')
 
-app.engine('html', require('ejs').renderFile);
 app.use(morgan('combined'))
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
-    mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
-    mongoURLLabel = "";
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
+
+
 
 mongoose.connect('mongodb://tristyntech:password@ds135547.mlab.com:35547/blogtestdb')
 
 var todoSchema = new mongoose.Schema({
-  text: String,
-  done: Boolean
+text: String,
+done: Boolean
 })
 
 
 var blogPostSchema = new mongoose.Schema({
-  image: String,
-  title: String,
-  headline: String,
-  body: String,
-  author: String,
+image: String,
+title: String,
+headline: String,
+body: String,
+author: String,
 })
 
 var Blog = mongoose.model('Blog', blogPostSchema)
 var Todo = mongoose.model('Todo', todoSchema)
 
 
-app.get('/test', (req, res)=>{
-  Todo.find({})
-  .then((todos) => {
-    res.send(todos);
-  })
+app.post('/addBlog', (req, res) => {
+var title = req.body.title
+var body = req.body.body
+var image = req.body.image
+var headline = req.body.headline
+
+console.log('#################working')
+var newPost = new Blog({
+title: title,
+body: body,
+image: image,
+headline: headline,
+author: "Geoff Dennis"
+})
+console.log('newPost', newPost)
+newPost.save()
+.then((resp) => {
+res.send(resp)
+})
 })
 
-app.get('/', (req, res)=>{
-  res.send('dinner')
+app.post('/todos/:text', (req, res) => {
+var t = req.params.text
+//pop
+var newTodo = new Todo({
+text: t,
+done: false
+})
+
+newTodo.save()
+.then((res)=>{
+  res.json(res)
+})
+})
+
+
+app.get('/getBlogs', (req, res) =>{
+Blog.find({})
+.then((resp) => {
+res.json(resp);
+})
+})
+
+app.get('/lulu', (req, res)=> {
+Blog.find({})
+.then((todo)=>{
+res.json(todo)
+})
+})
+
+app.post("/login", (req, res) => {
+var username = req.body.username;
+var password = req.body.password
+if (username === 'theg@gdennis.com' && password==='password') {
+res.json('success')
+} else {
+res.json('fail')
+}
 })
 
 
@@ -51,9 +99,8 @@ app.use(function(err, req, res, next){
   res.status(500).send('Something bad happened!');
 });
 
-initDb(function(err){
-  console.log('Error connecting to Mongo. Message:\n'+err);
-});
+
+// app.listen(3000, ()=>(console.log('running')))
 
 app.listen(port, ip);
 console.log('Server running on http://%s:%s', ip, port);
